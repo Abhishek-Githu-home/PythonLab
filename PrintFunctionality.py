09c2484e516a8c6fa0caa44c6cf4a42bf38fd884
Abhishek K M <abhishekabhi6823@gmail.com>
2025-09-28 09:00:46 +0530

    Basic Print Functionality in Python

---
+++ PrintFunctionality.py
@@ -0,0 +1,42 @@
+
+#Print Functionality (Printing the output)
+
+print("Hello world")
+#Expected output = Hello world
+
+print("Hello world, This is Abhishek K M")
+#Expected output = Hello world, This is Abhishek K M
+
+print("Hello world \n This is Abhishek K M \n He is mastering in python")
+#Expected output = Hello world
+#This is Abhishek K M
+#He is mastering in python
+
+print("Hello world" , "This is Abhishek K M",  "he is mastering in python", sep=" - ")
+#Expected output = Hello world - This is Abhishek K M - he is mastering in python
+
+print("Hello world", "This is Abhishek K M", "He is Mastering in python", sep=" and ")
+#Expected output = Hello world and This is Abhishek K M and He is Mastering in python
+
+print("Hello world", end=" ")
+print("This is Abhishek K M")
+#Expected output = Hello world This is Abhishek K M
+
+print("Hello world", end=" ** ")
+print("This is Abhishek K M")
+#Expected output = Hello world ** This is Abhishek K M
+
+
+print("Programming", "Essentials", "in", sep = "***", end="...")
+print("Python")
+#Expected output = Programming***Essentials***in...Python
+
+print("***pyramid***")
+print("   ******")
+print("    ****")
+print("     **")
+#Expected output = ***pyramid***
+   #******
+    #****
+     #**
+
