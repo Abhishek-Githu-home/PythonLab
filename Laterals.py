a306bae0972c272980983cee986d3303fa18097a
Abhishek K M <abhishekabhi6823@gmail.com>
2025-09-29 07:38:57 +0530

    Laterals of Python

---
+++ Laterals.py
@@ -0,0 +1,56 @@
+########Laterals
+
+#######Integer
+print(2)
+print("2")
+#Expected output = 2
+
+######Octal and Hexadecimal Numbers
+print(0x123) #0x - Hexadecimal Number
+print(0o342) #0o - Octal Number
+
+######Float
+print(0.4) #Floating-Number
+#Expected output = 0.4
+print("4.4") #Floating-Number
+#Expected output = 4.4
+print("44") #Integer-Number
+#Expected output = 44
+
+#######Exponent
+print(3e8) #e is exponent
+#Expected output = 300000000.0
+print(4E5) #E is exponent
+#Expected output = 400000.0
+print(0.0000000000000000000001)
+#Expected output = 1e-22
+print(0.00005)
+#Expected output = 5e-05
+
+#####Strings
+print("This is \"Abhishek\"") #Backslash acts as escaper
+#Expected output = This is "Abhishek"
+print("This is 'Abhishek'")
+#Expected output = This is 'Abhishek'
+print("I\'m Abhi")
+#Expected output = I'm Abhi
+
+###Boolean Values
+#True and False
+print(True == False)
+print(True > False)
+print(False > True)
+
+
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
+#Expected output : "I'm"
+               #""learning""
+               #"""Python"""
+
+###Binary Number
+##Formula = B x 2*0 + B x 2*1 + B x 2*2 + B x 2*3 
+#1011 Binary number = It's 11, because (2**0) + (2**1) + 0 + (2**3) = 11
+
+#11011 Binary number = 1 x 2*0 + 1 x 2*1 + 0 x 2*2 + 1 x 2*3 + 1 x 2*4 
+# 1 + 2 + 0 + 8 + 16  =  27
+
