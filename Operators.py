2fb1607d41c2f50d90a95c45c7c3788c5311a139
Abhishek K M <abhishekabhi6823@gmail.com>
2025-09-29 10:36:07 +0530

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,37 @@
+#####Data Operators#####
+
+###Addition (+)
+print(2+2)
+#Expected output : 4
+
+###Substraction (-)
+print(3-1)
+#Expected output : 2
+
+###Multiplication (*)
+print(2*5)
+#Expected output : 10
+
+####Division (/)
+print(50/2)
+#Expected output : 25.0
+
+###Floor Division (//)
+print(50//2)
+#Expected output : 25
+
+###Modulus - reminder (%)
+print(14%3)
+#Expected output : 2
+print(12 % 4.5)
+#Expected output : 3.0
+
+###Exponential
+print(2**3)
+#Expected output : 8
+
+#Priority wise operators
+print(2**3+8-9/2%3)
+#Expected output : 14.5
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+#Expected output : 10
